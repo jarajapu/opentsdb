891c11385e52b59795f14bf637b6e635a6989217
uday <uday@opentsdb.(none)>
2012-10-28 18:38:11 -0700

    pulled latest metricForm to fix Merge

--- src/tsd/client/MetricForm.java
+++ src/tsd/client/MetricForm.java
@@ -14,17 +14,22 @@ package tsd.client;
 
 import java.util.ArrayList;
 
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.DomEvent;
 import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Focusable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 final class MetricForm extends HorizontalPanel implements Focusable {
 
@@ -32,6 +37,13 @@ final class MetricForm extends HorizontalPanel implements Focusable {
     void onMetricChange(MetricForm widget);
   }
 
+  private static final String TSDB_ID_CLASS = "[-_./a-zA-Z0-9]";
+  private static final String TSDB_ID_RE = "^" + TSDB_ID_CLASS + "*$";
+  private static final String TSDB_TAGVALUE_RE =
+    "^(\\*?"                                       // a `*' wildcard or nothing
+    + "|" + TSDB_ID_CLASS + "+(\\|" + TSDB_ID_CLASS + "+)*)$"; // `foo|bar|...'
+
+  private final EventsHandler events_handler;
   private MetricChangeHandler metric_change_handler;
 
   private final CheckBox downsample = new CheckBox("Downsample");
@@ -41,11 +53,11 @@ final class MetricForm extends HorizontalPanel implements Focusable {
   private final CheckBox x1y2 = new CheckBox("Right Axis");
   private final ListBox aggregators = new ListBox();
   private final ValidatedTextBox metric = new ValidatedTextBox();
-  private TagsPanel tagsPanel;
+  private final FlexTable tagtable = new FlexTable();
 
   public MetricForm(final EventsHandler handler) {
+    events_handler = handler;
     setupDownsampleWidgets();
-    tagsPanel = new TagsPanel(handler);
     downsample.addClickHandler(handler);
     downsampler.addChangeHandler(handler);
     interval.addBlurHandler(handler);
@@ -67,7 +79,7 @@ final class MetricForm extends HorizontalPanel implements Focusable {
       metric.addKeyPressHandler(metric_handler);
     }
 
-    metric.setValidationRegexp(ClientConstants.TSDB_ID_RE);
+    metric.setValidationRegexp(TSDB_ID_RE);
     assembleUi();
   }
 
@@ -179,15 +191,10 @@ final class MetricForm extends HorizontalPanel implements Focusable {
   public CheckBox x1y2() {
     return x1y2;
   }
-  
-  public void autoSuggestTag(String tag) {
-    tagsPanel.autoSuggestTag(tag);
-  }
 
   private void assembleUi() {
     setWidth("100%");
     {  // Left hand-side panel.
-      final VerticalPanel leftPanel = new VerticalPanel();
       final HorizontalPanel hbox = new HorizontalPanel();
       final InlineLabel l = new InlineLabel();
       l.setText("Metric:");
@@ -199,17 +206,11 @@ final class MetricForm extends HorizontalPanel implements Focusable {
       hbox.setWidth("100%");
       metric.setWidth("100%");
 
-      //xx HEAD
       tagtable.setWidget(0, 0, hbox);
       tagtable.getFlexCellFormatter().setColSpan(0, 0, 3);
       addTag();
       tagtable.setText(1, 0, "Tags");
       add(tagtable);
-      // -----
-      leftPanel.add(hbox);
-      leftPanel.add(tagsPanel);
-      add(leftPanel);
-      // opentsdb-16
     }
     {  // Right hand-side panel.
       final VerticalPanel vbox = new VerticalPanel();
@@ -267,11 +268,11 @@ final class MetricForm extends HorizontalPanel implements Focusable {
     }
     url.append(':').append(metric);
     {
-      final String[][] tags = tagsPanel.getTags();
+      final int ntags = getNumTags();
       url.append('{');
-      for (int i = 0; i < tags.length; i++) {
-        final String tagname = tags[i][0];
-        final String tagvalue = tags[i][1];
+      for (int tag = 0; tag < ntags; tag++) {
+        final String tagname = getTagName(tag);
+        final String tagvalue = getTagValue(tag);
         if (tagname.isEmpty() || tagvalue.isEmpty()) {
           continue;
         }
